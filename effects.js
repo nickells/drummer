0693cbae8d4fb1239057d0da7db18c9decf61a11
Nick Ellsworth <ellsworth.nick@gmail.com>
2019-02-01 01:33:15 -0500

    compressor wip

--- effects.js
+++ effects.js
@@ -8,7 +8,6 @@ const convolver = context.createConvolver();
 // connect the nodes together
 distortion.connect(biquadFilter)
 // distortion.connect(biquadFilter);
-biquadFilter.connect(context.destination);
 
 // Manipulate the Biquad filter
 biquadFilter.type = "lowpass";
@@ -20,6 +19,19 @@ const setFilterFrequency = (value) => {
 const setFilterResonance = (value) => {
   biquadFilter.Q.value = value;
 }
+
+const compressor = context.createDynamicsCompressor();
+compressor.threshold.setValueAtTime(0, context.currentTime);
+compressor.knee.setValueAtTime(40, context.currentTime);
+compressor.ratio.setValueAtTime(12, context.currentTime);
+compressor.attack.setValueAtTime(0, context.currentTime);
+compressor.release.setValueAtTime(0.25, context.currentTime);
+
+window.compressor = compressor
+
+// connect the AudioBufferSourceNode to the destination
+biquadFilter.connect(compressor);
+compressor.connect(context.destination);
 
 setFilterFrequency(PARAMS.FILTER_FREQUENCY.value)
 setFilterResonance(PARAMS.FILTER_RESONANCE.value)
